8f12b510d0cba04a4b0cfbdc9fbf34d23f295611
graemeblair <graeme.blair@gmail.com>
2015-05-11 21:47:30 -0400

    removed onAttach

--- R/onAttach.R
+++
@@ -1,8 +0,0 @@
-".onAttach" <- function(lib, pkg) {
-  mylib <- dirname(system.file(package = pkg))
-  title <- packageDescription(pkg, lib.loc = mylib)$Title
-  ver <- packageDescription(pkg, lib.loc = mylib)$Version
-  author <- packageDescription(pkg, lib.loc = mylib)$Author
-  packageStartupMessage(pkg, ": ", title, "\nVersion: ", ver, "\nAuthor: ", author, "\n")
-}
-
